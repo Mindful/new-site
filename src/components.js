06f3684605e60ebef94db9d0cec5191d61766a00
Joshua Tanner <mindful.jt@gmail.com>
2021-11-13 21:21:17 +0900

    refactor, work on home page

---
+++ src/components.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Link } from "gatsby"
+import github from "./images/scaled-github.svg";
+import se from "./images/scaled-se.svg";
+import linkedin from "./images/scaled-linkedin.svg";
+import scholar from "./images/scaled-scholar.svg";
+import email from "./images/scaled-email.svg";
+
+// https://www.gatsbyjs.com/blog/2020-02-19-how-to-build-multilingual-sites-with-gatsby/
+export function LanguageSelector({lang, location, className}) {
+    return {
+        'en': (
+            <Link className={className} to={`/ja${location.pathname}`}>
+                日本語
+            </Link>
+        ),
+        'ja': (
+            <Link className={className} to={location.pathname.replace("/" + lang + "/", "/")}>
+                english
+            </Link>
+        )
+    }[lang]
+}
+
+export function Header({lang, location}) {
+    return <nav className="flex bg-green p-0 h-16">
+        <div className="flex items-center flex-shrink-0 text-white mr-6 ml-6">
+            <span>Joshua Tanner</span>
+        </div>
+        <div style={{borderLeft: "1px solid white", height: "100%"}} className="mr-6"/>
+        <div className="w-full flex items-center text-white">
+                <a href="#responsive-header" className="mr-4">
+                    Software
+                </a>
+                <a href="#responsive-header" className=" mr-4">
+                    Translation
+                </a>
+                <a href="#responsive-header" className="mr-4">
+                    Blog
+                </a>
+                <LanguageSelector lang={lang} location={location} className="absolute right-4"/>
+        </div>
+    </nav>
+}
+
+export function Textbox({lang, location, children}) {
+    return <div className="bg-white flex m-10 p-14 shadowed w-full">
+        {children}
+    </div>
+}
+
+export function Sidebar() {
+    return <div className="flex-col items-center bg-transparent h-full pr-5 pt-5">
+        <img src={github} alt="github" className="pt-2 pb-2"/>
+        <img src={se} alt="stack exchange" className="pt-2 pb-2"/>
+        <img src={linkedin} alt="linkedin" className="pt-2 pb-2"/>
+        <img src={scholar} alt="google scholar" className="pt-2 pb-2"/>
+        <img src={email} alt="email" className="pt-2 pb-2"/>
+    </div>
+}
+
+
+
+
--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -1,59 +1,22 @@
 import React from "react"
-import github from "../images/scaled-github.svg"
-import se from "../images/scaled-se.svg"
-import linkedin from "../images/scaled-linkedin.svg"
-import scholar from "../images/scaled-scholar.svg"
-import email from "../images/scaled-email.svg"
-import LanguageSelector from "../components/language_selector"
+import {Header, LanguageSelector, Sidebar, Textbox} from "../components"
+import portrait from "../images/portrait.jpg"
 
-function header() {
-  return <nav className="flex items-center justify-between flex-wrap bg-green p-0 h-16">
-    <div className="flex items-center flex-shrink-0 text-white mr-6 ml-6">
-      <span>Joshua Tanner</span>
-    </div>
-    <div style={{borderLeft: "1px solid white", height: "100%"}} className="mr-6"/>
-    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-white">
-      <div className="text-sm lg:flex-grow">
-        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0  mr-4">
-          Software
-        </a>
-        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
-          Translation
-        </a>
-        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0">
-          Blog
-        </a>
-      </div>
-    </div>
-  </nav>
-}
 
-function textbox() {
-  return <div className="bg-white flex m-10 p-5 shadowed w-full">
-    oogity boogity
-  </div>
-}
-
-function sidebar() {
-  return <div className="flex-col items-center bg-transparent h-full pr-5 pt-5">
-    <img src={github} alt="github" className="pt-2 pb-2"/>
-    <img src={se} alt="stack exchange" className="pt-2 pb-2"/>
-    <img src={linkedin} alt="linkedin" className="pt-2 pb-2"/>
-    <img src={scholar} alt="google scholar" className="pt-2 pb-2"/>
-    <img src={email} alt="email" className="pt-2 pb-2"/>
-  </div>
-}
 
 
 export default function IndexPage({data, location}) {
-  return (
-      <main>
-        {header()}
-        <div className="flex flex-row">
-          <LanguageSelector location={location} lang="en"/>
-          {textbox()}
-          {sidebar()}
-        </div>
-      </main>
-  )
+    let lang = "en";
+    return (
+        <main>
+            <Header location={location} lang={lang}/>
+            <div className="flex flex-row">
+                <Textbox>
+                    <img className="flex flex-shrink rounded-full h-80 w-80" src={portrait}/>
+                    <p>Under construction</p>
+                </Textbox>
+                <Sidebar/>
+            </div>
+        </main>
+    )
 }
