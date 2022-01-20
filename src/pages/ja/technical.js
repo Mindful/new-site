baca687cdff661e9347b50f30fb7e39b79984f4b
Joshua Tanner <mindful.jt@gmail.com>
2022-01-20 19:15:19 +0900

    fix japanese

--- src/pages/ja/technical.js
+++ src/pages/ja/technical.js
@@ -15,12 +15,12 @@ export default function IndexPage({data, location}) {
                     <div><img src={resume} alt="github" className="relative translate-x-1/2"/></div>
                 </a>
                 <Heading>自然言語処理</Heading>
-                <p>実際の問題を解決することに焦点を置きながら、様々な自然言語処理の技術を生かし研究や開発を行います。よく使うライブラリ野中には、
+                <p>実際の問題を解決することに焦点を置きながら、様々な自然言語処理の技術を生かし研究や開発を行います。よく使うライブラリの中には、
                     spaCy、Pytorch、scikit-learnなどがあります。</p>
                 <hr className={'my-8'}/>
                 <Heading>ソフトウェアエンジニアリング</Heading>
                 <p>ソフト開発ではバックエンドを専門に、大手企業からベンチャーまででスケーラブルかつ安定したバックエンドを様々なプログラミング言語で
-                    開発してきました。 必要とあれば、フロントエンドやモバイル開発もできます。
+                    開発してきました。 必要に応じてフロントエンドやモバイル開発もできます。
                 </p>
             </ContentBox>
         </Layout>
--- src/pages/ja/translation.js
+++ src/pages/ja/translation.js
@@ -15,10 +15,10 @@ export default function IndexPage({data, location}) {
                     <div><img src={resume} alt="github" className="relative translate-x-1/2"/></div>
                 </a>
                 <Heading>通訳</Heading>
-                <p>英和の逐次通訳者として活動しています。経験の多くは国際会議に来客された方の専属通訳になりますが、分野の広範囲にわたって通訳できます。</p>
+                <p>英和の逐次通訳者として活動しています。経験の多くは国際会議に来客された方の専属通訳になりますが、広範囲の分野にわたって通訳できます。</p>
                 <hr className={'my-8'}/>
                 <Heading>翻訳</Heading>
-                <p>書類やアプリなどの翻訳もできます。技術者としての職歴のおかげでソフトウェア開発と自然言語処理に関係する技術翻訳なら適任です。</p>
+                <p>書類やアプリなどの翻訳もできます。技術者としての職歴のおかげでソフトウェア開発と自然言語処理に関係する技術翻訳に適任です。</p>
             </ContentBox>
         </Layout>
     )
